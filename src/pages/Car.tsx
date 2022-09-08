016a5404d50cfd2452be6da87813e3a933a019f2
Ainsdev <ainsn1654@gmail.com>
2022-09-08 20:26:49 -0300

    finish

--- src/pages/Car.tsx
+++ src/pages/Car.tsx
@@ -1,10 +1,37 @@
-import { motion } from "framer-motion";
-import { useState } from "React";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+/* import { useEffect } from "react"; */
+import { useInView } from "react-intersection-observer";
 import BackButton from "../components/BackButton";
 import { TeslaCarType } from "../interfaces/interfaces&types";
 
 function Car(props: TeslaCarType) {
-    const firstImage = props.bgImages[0]
+    const { ref, inView } = useInView();
+    const animation = useAnimation();
+    const animationS = useAnimation();
+    useEffect(() => {
+        if (inView) {
+            animation.start({
+                x: 0,
+                transition: {
+                    type: 'spring', duration: 2, bounce: 0.2
+                }
+            })
+            animationS.start({
+                x: 0,
+                transition: {
+                    type: 'spring', duration: 2, bounce: 0.2
+                }
+            })
+        } else {
+            animation.start({
+                x: '-100vw'
+            })
+            animationS.start({
+                x: '100vw'
+            })
+        }
+    })
     return (
         <main className="scroll-smooth snap-y snap-mandatory overflow-hidden" >
             <section className={`text-white snap-always snap-start relative w-screen h-screen flex flex-col justify-between items-center bg-center bg-cover`} style={{ backgroundImage: `url(${props.bgImages[0]})` }}>
@@ -39,26 +66,26 @@ function Car(props: TeslaCarType) {
             <section className={`text-white snap-always snap-start relative w-screen h-screen flex flex-col justify-between items-center bg-center bg-cover`} style={{ backgroundImage: `url(${props.bgImages[1]})` }}>
                 <motion.h1 animate={{}} className='text-5xl font-bold pt-24 text-white'>Aerodinamic</motion.h1>
             </section>
-            <section className="snap-always snap-start w-screen h-max p-40 bg-black flex justify-center items-center">
+            <section ref={ref} className="snap-always snap-start w-screen h-max p-40 bg-black flex justify-center items-center">
                 <div className="grid sm:grid-cols-2 gap-y-5 grid-rows-3 text-white text-center">
                     <article className="w-[60vh] h-max order-1">
-                        <motion.div transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '-100vw' }} animate={{ x: 0 }}><img src={props.data[0].photo} /></motion.div>
+                        <motion.div animate={animation}><img src={props.data[0].photo} /></motion.div>
                     </article>
-                    <motion.article transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '100vw' }} animate={{ x: 0 }} className="w-[60vh] h-max p-5 order-2">
+                    <motion.article animate={animationS} className="w-[60vh] h-max p-5 order-2">
                         <h1 className="text-xl font-bold pb-12">{props.data[0].title}</h1>
                         <p className="text-lg px-20">{props.data[0].text}</p>
                     </motion.article>
-                    <motion.article transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '100vw' }} animate={{ x: 0 }} className="w-[60vh] h-max order-4 sm:order-3">
+                    <motion.article animate={animation} className="w-[60vh] h-max order-4 sm:order-3">
                         <h1 className="text-xl font-bold pb-12">{props.data[1].title}</h1>
                         <p className="text-lg px-20">{props.data[1].text}</p>
                     </motion.article>
                     <motion.article className="w-[60vh] h-max p-5 order-3 sm:order-4">
-                        <motion.div transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '-100vw' }} animate={{ x: 0 }} ><img src={props.data[1].photo} /></motion.div>
+                        <motion.div animate={animationS} ><img src={props.data[1].photo} /></motion.div>
                     </motion.article>
                     <motion.article className="w-[60vh] h-max order-4">
-                        <motion.div transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '100vw' }} animate={{ x: 0 }}><img src={props.data[2].photo} /></motion.div>
+                        <motion.div animate={animation}><img src={props.data[2].photo} /></motion.div>
                     </motion.article>
-                    <motion.article transition={{ type: 'spring', duration: 1, bounce: 0.4 }} initial={{ x: '100vw' }} animate={{ x: 0 }} className="w-[60vh] h-max p-5 order-5">
+                    <motion.article animate={animationS} className="w-[60vh] h-max p-5 order-5">
                         <h1 className="text-xl font-bold pb-12">{props.data[2].title}</h1>
                         <p className="text-lg px-20">{props.data[2].text}</p>
                     </motion.article>
